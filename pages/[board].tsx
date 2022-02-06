acb0fc0ca2beb6807313e3d4cdfa377d064c91df
Alejandro <alex.aleman.public@gmail.com>
2022-02-06 15:13:42 -0600

    Added dynamic routing for boards

--- pages/[board].tsx
+++ pages/[board].tsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import type { NextPage } from 'next';
+import { useRouter } from 'next/router';
 
 const Board: NextPage = () => {
+	const router = useRouter();
+	const { board } = router.query;
+
 	return (
 		<div>
-			<h1>board</h1>
+			<h1>{ board }</h1>
 		</div>
 	);
 };
